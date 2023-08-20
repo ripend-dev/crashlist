a38032b1d21cbe8d9aa2af65c288e69c953e6517
Ripend <142525915+ripend-dev@users.noreply.github.com>
2023-08-20 17:46:00 +0200

    Update crashes.lua

--- crashes.lua
+++ crashes.lua
@@ -2,7 +2,7 @@ local chat = function(msg) game.Players:Chat(msg) end
 
 local crash = {
     ['freezecrash'] = function()
-        for i = 1, 200 do
+        for i = 1, 100 do
             chat('freeze '..string.rep(' all', 40))
             task.wait()
         end
@@ -11,7 +11,7 @@ local crash = {
         end
     end,
     ['hatcrash'] = function()
-        for i = 1, 650 do
+        for i = 1, 100 do
             chat('hat all 000000000000000000000000000000012597599966')
             task.wait()
         end
@@ -20,7 +20,7 @@ local crash = {
         end
     end,
     ['shieldcrash'] = function()
-        for i = 1, 200 do
+        for i = 1, 100 do
             chat('shield/all/all/all/all/all/all/all')
             task.wait()
         end
